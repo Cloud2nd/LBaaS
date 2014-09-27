f39e804e77ff2ea201a16ed1a78d36ada64341e7
Jungsu Han <jshan@osci.kr>
2014-09-28 01:40:18 +0900

    Modify Login Controller

--- baas-server/baas-server/src/main/java/com/exactsix/mibaas/login/LoginController.java
+++ baas-server/baas-server/src/main/java/com/exactsix/mibaas/login/LoginController.java
@@ -2,6 +2,7 @@ package com.exactsix.mibaas.login;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.exactsix.mibaas.common.response.RestResponse;
+import com.exactsix.mibaas.login.service.LoginService;
 
 /**
  * <pre>
@@ -28,10 +30,12 @@ import com.exactsix.mibaas.common.response.RestResponse;
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/users", method = RequestMethod.GET)
-	public @ResponseBody RestResponse list() {
-		//
-		return null;
+	@Autowired
+	private LoginService loginService;
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public @ResponseBody RestResponse login() {
+		return loginService.getLogin();
 	}
 
 }
