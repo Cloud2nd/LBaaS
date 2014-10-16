5414f4da83ed3b08834693d6f34d84c0a2fd1cef
jshan@osci.kr <wjdtn#1212>
2014-10-16 23:07:22 +0900

    HttpReqeust

--- baas-server/lbaas-auth/src/main/java/com/exactsix/mibaas/user/UserController.java
+++ baas-server/lbaas-auth/src/main/java/com/exactsix/mibaas/user/UserController.java
@@ -1,5 +1,12 @@
 package com.exactsix.mibaas.user;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,9 +38,44 @@ public class UserController {
 
 	@Autowired
 	private LoginService loginService;
+	
+	@Autowired
+	private HttpServletRequest context;
 
 	@RequestMapping(value = "/usermanagement/login", method = RequestMethod.POST)
 	public @ResponseBody RestResponse login(@RequestBody UserDto userDto) {
+		
+		String body = null;
+	    StringBuilder stringBuilder = new StringBuilder();
+	    BufferedReader bufferedReader = null;
+
+	    try {
+	        InputStream inputStream = request.getInputStream();
+	        if (inputStream != null) {
+	            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+	            char[] charBuffer = new char[128];
+	            int bytesRead = -1;
+	            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+	                stringBuilder.append(charBuffer, 0, bytesRead);
+	            }
+	        } else {
+	            stringBuilder.append("");
+	        }
+	    } catch (IOException ex) {
+	        throw ex;
+	    } finally {
+	        if (bufferedReader != null) {
+	            try {
+	                bufferedReader.close();
+	            } catch (IOException ex) {
+	                throw ex;
+	            }
+	        }
+	    }
+
+	    body = stringBuilder.toString();
+	    
+	    System.out.println(body);
 		return loginService.getLogin(userDto);
 	}
 
@@ -44,6 +86,11 @@ public class UserController {
 
 	@RequestMapping(value = "/usermanagement/{username}", method = RequestMethod.POST)
 	public @ResponseBody RestResponse userList(@RequestBody UserDto userDto) {
+		return null;
+	}
+	
+	@RequestMapping(value = "/usermanagement/test", method = RequestMethod.POST)
+	public @ResponseBody RestResponse device() {
 		return null;
 	}
 
