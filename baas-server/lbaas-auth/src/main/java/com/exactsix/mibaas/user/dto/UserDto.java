58f029652979d34bc587c664f2c3548fa35f55f4
Jungsu Han <jshan@osci.kr>
2014-10-23 15:49:00 +0900

    Dao modify

--- baas-server/lbaas-auth/src/main/java/com/exactsix/mibaas/user/dto/UserDto.java
+++ baas-server/lbaas-auth/src/main/java/com/exactsix/mibaas/user/dto/UserDto.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-@JsonIgnoreProperties
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_NULL)
 public class UserDto {
 
